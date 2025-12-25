408b15480acdae894bf9cee2e954ddbf57c9758e
karim <karem.mohd2@gmail.com>
2025-12-25 09:12:18 +0200

    chore: add @nuxtjs/i18n dependency to package.json

--- nuxt-frontend/nuxt.config.ts
+++ nuxt-frontend/nuxt.config.ts
@@ -6,6 +6,36 @@ export default defineNuxtConfig({
     compatibilityVersion: 4,
   },
 
+  modules: ['@nuxtjs/i18n'],
+
+  i18n: {
+    locales: [
+      {
+        code: 'ar',
+        iso: 'ar-SA',
+        name: 'العربية',
+        dir: 'rtl',
+        file: 'ar.json'
+      },
+      {
+        code: 'en',
+        iso: 'en-US',
+        name: 'English',
+        dir: 'ltr',
+        file: 'en.json'
+      }
+    ],
+    defaultLocale: 'ar',
+    strategy: 'no_prefix',
+    lazy: true,
+    langDir: 'locales',
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: 'i18n_redirected',
+      redirectOn: 'root',
+    }
+  },
+
   runtimeConfig: {
     backendUrl: process.env.BACKEND_URL || "http://localhost:3001",
   },
